1afe06c0127c94b5db7bc062c6189c9ee49eeadb
Umar Musa Halliru <umusa5801@gmail.com>
2025-07-14 23:37:20 +0100

    'M'

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://gradelink.onrender.com/api';
 
 export const getStudents = async () => {
   try {
